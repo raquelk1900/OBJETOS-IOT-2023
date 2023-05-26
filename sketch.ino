4a8bb8f8a3bdbf6b6f655bc6c709a958c66fcd9b
raquelk1900 <125435344+raquelk1900@users.noreply.github.com>
2023-05-25 21:16:33 -0300

    Add files via upload

---
+++ sketch.ino
@@ -0,0 +1,60 @@
+#include "OneWire.h"
+#include "DallasTemperature.h"
+#include "EspMQTTClient.h"
+
+EspMQTTClient client(
+  "Wokwi-GUEST",         // SSID WiFi
+  "",                    // PAssword WiFi
+  "test.mosquitto.org",  // MQTT Broker
+  "mqtt-wokwi",          // Client
+  1883                   // MQTT port
+);
+
+
+// GPIO where the DS18B20 is connected to
+OneWire oneWire(5);         
+// Pass our oneWire reference to Dallas Temperature sensor 
+DallasTemperature sensors(&oneWire);
+// Temperature value
+float temp;
+
+
+void setup() {
+  // Start o DS18B20 sensor
+  Serial.begin(115200);
+  delay(2);
+  sensors.begin();
+  delay(20);
+
+  client.enableDebuggingMessages(); // Enable debugging messages sent to serial output
+  client.enableHTTPWebUpdater(); // Enable the web updater. User and password default to values of MQTTUsername and MQTTPassword. These can be overridded with enableHTTPWebUpdater("user", "password").
+  client.enableOTA(); // Enable OTA (Over The Air) updates. Password defaults to MQTTPassword. Port is the default OTA port. Can be overridden with enableOTA("password", port).
+  client.enableLastWillMessage("TestClient/lastwill", "Offline");
+}
+
+void lerDados() {
+  
+  sensors.requestTemperatures();
+  temp = sensors.getTempCByIndex(0);
+  Serial.println("temp: " + String(temp) + "°C");
+  Serial.println("---");
+  client.publish("topicowokwi/temp", String(temp) + "°C");
+
+}
+
+
+void onConnectionEstablished()
+{
+  // Subscribe no "topicowokwi/msgRecebida/#" e mostra a mensagem recebida na Serial
+  client.subscribe("topicowokwi/msgRecebida/#", [](const String & topic, const String & payload) {
+    Serial.println("Mensagem recebida no topic: " + topic + ", payload: " + payload);
+  });
+
+
+  lerDados();
+}
+
+void loop() {
+  client.loop(); // Executa em loop
+
+}
